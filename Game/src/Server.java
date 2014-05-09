6b99bd8d3c2661c9f4c9f3f0d201b12a84b10fef
Anna <annagudkova94@gmail.com>
2014-05-09 14:38:20 +0600

    Update Server.java

--- Game/src/Server.java
+++ Game/src/Server.java
@@ -4,12 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Server extends Close {
 	private static Socket socket;
@@ -19,62 +19,67 @@ public class Server extends Close {
 	private static InputStream instream;
 	Request m_request;
 	Response m_response;
+	volatile static HashMap<Integer, Thread> clients = new HashMap<Integer, Thread>();
+
 	public static void main(String[] args) throws CodeException, IOException {
 		File file=new File("world.txt");
-		instream = new FileInputStream(file);
 		try {
-			server = new ServerSocket(17396);
+			server = new ServerSocket(17376);
+			instream = new FileInputStream(file);
 			socket = server.accept();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		PrintableWoodLoader W = new PrintableWoodLoader();
-		PrintableWood wood = W.PrintableWoodLoad(instream,System.out);			
-		LinkedList<Point> points = new LinkedList<Point>();
-		Synchronizer synchronizer = new Synchronizer();
-		Thread thread;
-		Thread threadSynchronizer = new Thread(synchronizer);
-		points.add(new Point(1,2));
-		points.add(new Point(1,3));
-		points.add(new Point(1,4));
-		points.add(new Point(1,5));
-		points.add(new Point(2,7));
-		points.add(new Point(6,7));			
-		try {
-			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-		}
-		catch (IOException e2) {
-			e2.printStackTrace();
-		}
-		try {
-		threadSynchronizer.start();
-			System.out.println("Server started");
-			while (true) {
-					Stop s = new Stop();
-					s.start();
-				    if (s.flag) {
-					 System.out.println("Server stopped");
-					 		threadSynchronizer.interrupt();
-					 		throw new  Stopper();
-					 
-					 					}
-					try {
-					thread = new Thread(new ThreadsServer(socket, wood, points, synchronizer));
-					thread.start();	 			
-					}
+			PrintableWoodLoader W = new PrintableWoodLoader();
+			PrintableWood wood = W.PrintableWoodLoad(instream,System.out);			
+			LinkedList<Point> points = new LinkedList<Point>();
+			Synchronizer synchronizer = new Synchronizer(clients);
+			Thread threadSynchronizer = new Thread(synchronizer);
 			
-				catch (SocketTimeoutException e) {
+			points.add(new Point(1,2));
+			points.add(new Point(1,3));
+			points.add(new Point(1,4));
+			points.add(new Point(1,5));
+			points.add(new Point(2,7));
+			points.add(new Point(6,7));		
+
+			threadSynchronizer.start();
+			Stop s = new Stop();
+			s.start();
+
+			server.setSoTimeout(1000);
+
+			while (true) {
+				Integer threadID;
+				Thread thread;
+				if (s.flag) {
+					System.out.println("Server stopped");
+					throw new Stopper("Server stopped");
+				}
+				try {
+					Random random = new Random();
+					threadID = Math.abs(random.nextInt());
+					thread = new Thread(new ThreadsServer(server.accept(), wood, clients, points, threadID));
+				} catch (SocketTimeoutException e) {
 					continue;
 				}
-		try {
-				Thread.sleep(200);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}	}
+				synchronized (clients) {
+					clients.put(threadID, thread);
+				}					
+				thread.start();
+				if (clients.size() == 1) {
+					System.out.println(clients.size() + " mouse in the wood");
+				}
+				else {
+					System.out.println(clients.size() + " mouses in the wood");
+				}
 			}
+		}
+
 		catch (IOException e) {
 			e.printStackTrace();
+		}
+		catch(Exception e) {
+			tryClose(server);
+			tryClose(instream);
+			System.exit(0);
 		}
 		finally {
 			tryClose(server);
