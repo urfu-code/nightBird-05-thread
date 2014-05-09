8946b5d6fdce028829dac180802920d156c69343
Anna <annagudkova94@gmail.com>
2014-05-09 14:39:24 +0600

    Update Synchronizer.java

--- Game/src/Synchronizer.java
+++ Game/src/Synchronizer.java
@@ -1,20 +1,29 @@
+import java.util.HashMap;
+import java.util.Iterator;
+
 
 public class Synchronizer implements Runnable {
 
-	public Synchronizer () {}
+	private HashMap<Integer, Thread> сlients;
+
+	public Synchronizer(HashMap<Integer, Thread> сlientsList) {
+		this.сlients = сlientsList;
+	}
 
 	@Override
 	public void run() {
-		while (true) {
-			try {
-				synchronized (this) {
-					notifyAll();
+		while (true) {			
+			Iterator<Integer> keySetIterator = сlients.keySet().iterator();
+			while (keySetIterator.hasNext()) {
+				Integer key = keySetIterator.next();
+				synchronized (сlients.get(key)) {
+					сlients.get(key).notify();
+				}		  
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
-				Thread.sleep(500);
-			}
-			catch (InterruptedException e) {
-				e.printStackTrace();
-				break;
 			}
 		}
 	}
