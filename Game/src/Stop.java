a104dc4fd36a537e5f776ac0469ac304e408733d
Anna <annagudkova94@gmail.com>
2014-05-09 14:39:10 +0600

    Update Stop.java

--- Game/src/Stop.java
+++ Game/src/Stop.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Stop extends Thread {
 
 	volatile boolean flag;
+	
 	public Stop() {
 		this.flag = false;
 	}
 
 	@Override
 	public void run() {
-
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			String str = sc.nextLine();
@@ -19,9 +19,7 @@ public class Stop extends Thread {
 				sc.close();
 				return;				
 			}
-
 		}
-
 	}
 
 }
